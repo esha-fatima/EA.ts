a208f3a9ad847df455d318e80c3606935622f126
esha-fatima <eshafatima2001@gmail.com>
2023-01-15 19:53:07 +0300

    connected to cluster

---
+++ server/src/db/db.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const {MongoClient} = require('mongodb');
+export const database = async()=>{
+    const uri = "mongodb+srv://EshAhmed:Rapunzel26Ra@cluster0.okvrpxs.mongodb.net/test";
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        let databasesList = await client.db().admin().listDatabases();
+        let x = client.db("Users")
+        let f = "errr"
+        x.collection("testing").find({message:"hellows"}).toArray(function(err:any,res:any){
+            console.log(res);
+            f = res
+        })
+        console.log((x.collection("testing").find({message:"hellows"})).result)
+        console.log("Databases:", databasesList.databases);
+        //databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+        // Make the appropriate DB calls
+        return f
+ 
+    } catch (e) {
+        console.error(e);
+    } 
+  
+}
