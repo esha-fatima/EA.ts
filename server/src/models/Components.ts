c6ea35f6a6fcb11a04daeb6822b84ddc11603481
unknown <22100145@lums.edu.pk>
2023-01-17 19:19:31 +0500

    fixed some stuff

--- server/src/models/Components.ts
+++ server/src/models/Components.ts
@@ -3,26 +3,40 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-
+export interface GradedComponents {
+  rollNumber: string,
+  classParticipation?: number,
+  assignmentOne?: number,
+  assignmentTwo?: number,
+  assignmentThree?: number,
+  examOne?: number,
+  examTwo?: number,
+  examThree?: number,
+  examFour?: number,
+  //todo
+}
 
 const GradedComponents = new Schema({
   rollNumber: String,
-  classParticipation: {type:mongoose.Types.Decimal128, default:0.0},
-  assignmentOne:{type:mongoose.Types.Decimal128, default:0.0},
-  assignmentTwo : {type:mongoose.Types.Decimal128, default:0.0},
-  assignmentThree: {type:mongoose.Types.Decimal128, default:0.0},
-  examOne: {type:mongoose.Types.Decimal128, default:0.0},
-  examTwo: {type:mongoose.Types.Decimal128, default:0.0},
-  examThree: {type:mongoose.Types.Decimal128, default:0.0},
-  examFour: {type:mongoose.Types.Decimal128, default:0.0},
-  totalPercentage:{type:mongoose.Types.Decimal128, 
-                    default: function() {
-                                    return (this.assignmentOne+this.assignmentTwo+this.assignmentThree) 
-                               }
-            },
-
+  classParticipation: { type: mongoose.Types.Decimal128, default: 0.0 },
+  assignmentOne: { type: mongoose.Types.Decimal128, default: 0.0 },
+  assignmentTwo: { type: mongoose.Types.Decimal128, default: 0.0 },
+  assignmentThree: { type: mongoose.Types.Decimal128, default: 0.0 },
+  examOne: { type: mongoose.Types.Decimal128, default: 0.0 },
+  examTwo: { type: mongoose.Types.Decimal128, default: 0.0 },
+  examThree: { type: mongoose.Types.Decimal128, default: 0.0 },
+  examFour: { type: mongoose.Types.Decimal128, default: 0.0 },
+  totalPercentage: {
+    type: mongoose.Types.Decimal128,
+    default: function () {
+      return this.assignmentOne + this.assignmentTwo + this.assignmentThree;
+    },
+  },
 });
 
-const GradedComponentsModel = mongoose.model("GradedComponents", GradedComponents);
+const GradedComponentsModel = mongoose.model(
+  "GradedComponents",
+  GradedComponents
+);
 
 export default GradedComponentsModel;
--- server/src/models/Student.ts
+++ server/src/models/Student.ts
@@ -3,16 +3,23 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
+interface Student {
+  fullName: string;
+  rollNumber: string;
+  password: string;
+  status?: STUDENT_STATUS;
+}
+
 export enum STUDENT_STATUS {
   ACTIVE = "ACTIVE",
   WITHDRAWN = "WITHDRAWN",
 }
 
-const Student = new Schema({
-  fullName: {type:String},
+const Student = new Schema<Student>({
+  fullName: { type: String },
   rollNumber: String,
   password: String,
-  status: {type:STUDENT_STATUS, default: STUDENT_STATUS.ACTIVE},
+  status: { type: STUDENT_STATUS, default: STUDENT_STATUS.ACTIVE },
 });
 
 const StudentModel = mongoose.model("Student", Student);
