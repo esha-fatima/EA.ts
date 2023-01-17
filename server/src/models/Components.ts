914659a7e04b8d3b47d4e2cfb6a57a2bf1a7a5fc
esha-fatima <eshafatima2001@gmail.com>
2023-01-17 16:14:52 +0300

    added student and components

---
+++ server/src/models/Components.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+
+
+const GradedComponents = new Schema({
+  rollNumber: String,
+  classParticipation: {type:mongoose.Types.Decimal128, default:0.0},
+  assignmentOne:{type:mongoose.Types.Decimal128, default:0.0},
+  assignmentTwo : {type:mongoose.Types.Decimal128, default:0.0},
+  assignmentThree: {type:mongoose.Types.Decimal128, default:0.0},
+  examOne: {type:mongoose.Types.Decimal128, default:0.0},
+  examTwo: {type:mongoose.Types.Decimal128, default:0.0},
+  examThree: {type:mongoose.Types.Decimal128, default:0.0},
+  examFour: {type:mongoose.Types.Decimal128, default:0.0},
+  totalPercentage:{type:mongoose.Types.Decimal128, 
+                    default: function() {
+                                    return (this.assignmentOne+this.assignmentTwo+this.assignmentThree) 
+                               }
+            },
+
+});
+
+const StudentModel = mongoose.model("GradedComponents", GradedComponents);
+
+export default StudentModel;
